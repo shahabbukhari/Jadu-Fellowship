bb3c0f7c720338d94240dbee21230e1a4b993fec
shahabbukhari <shahabbukhari098@gmail.com>
2021-10-11 11:32:48 +0500

    Todo App

---
+++ Projects/03 - TodoApp/README.md
@@ -0,0 +1,14 @@
+# First Portfolio 
+This Repository contains all the source code my todo app which i have made for [Jadu Fellowship](https://github.com/Jadu-Fellowship).
+
+## Features
+In This todo ap you can perform following tasks:
+- Add/Remove/Edit Items
+- No. Pending task are listed below with Clear All Button
+- All of the tasks are stored in local storage its even on refresing the page all the task will stay in your memory.
+
+## Live View
+You can check the live view [here](https://shahabbukhari.github.io/Jadu-Fellowship/Projects/02%20-%20Portfolio/index.html):
+
+## Screenshots
+![Screenshot](todo.jpeg)
--- Projects/03 - TodoApp/logic.js
+++ Projects/03 - TodoApp/logic.js
@@ -1,11 +1,22 @@
 let textField = document.getElementById('todo_text');
 let submit = document.getElementById('add_todo');
 let place = document.getElementById('tasks');
+let tasks = document.getElementById('pending-task');
+let clearAll = document.getElementById('clearAll');
+let nodeCount = 0;
+let todoItems = []
 
-function adder() {
-    let value = textField.value;
+function adder(localValue) {
+    let value;
+    if(typeof localValue != 'object'){
+        value = localValue;
+        console.log("value added from local storage")
+    } else {
+        value = textField.value
+    }
     
     if(value != "") {
+        todoItems.push(value)
         let taskDiv = document.createElement('div');
         taskDiv.className = "todo_task"
 
@@ -13,29 +24,44 @@ function adder() {
         taskP.className = 'task_dis';
         taskP.innerHTML = value;
 
+        setLocal(todoItems)
 
         textField.value = ""
 
-        let dlt = document.createElement('button');
-        dlt.className = 'dlt_todo';
-        dlt.innerText = "Delete";
+        let dlt = document.createElement('i');
+        dlt.className = 'dlt_todo fas fa-trash';
         dlt.addEventListener('click', deleteTodo);
         
-        let edit = document.createElement('button');
-        edit.className = 'edit_todo';
-        edit.innerText = "Edit";
+        let edit = document.createElement('i');
+        edit.className = 'edit_todo fas fa-pen-square';
         edit.addEventListener('click', editor);
 
         taskDiv.appendChild(taskP)
-        taskDiv.appendChild(dlt)
         taskDiv.appendChild(edit)
+        taskDiv.appendChild(dlt)
         place.appendChild(taskDiv)
+
+        addCount(++nodeCount)
     }
     
 }
 function deleteTodo(e) {
     place.removeChild(this.parentNode || e.parentNode)
-    console.log('Deleted')
+    console.log('Deleted');
+
+    for (let i = 0; i < todoItems.length; i++) {
+        if(this.parentNode === undefined) {
+            if (todoItems[i] === e.parentNode.childNodes[0].innerHTML) {
+                todoItems.splice(i,1);
+                console.log("executed") 
+            }
+        } else if(todoItems[i] === this.parentNode.childNodes[0].innerHTML){
+            todoItems.splice(i,1);
+            console.log("executed")
+        }
+    }
+    setLocal(todoItems)
+    addCount(--nodeCount)
 }
 submit.addEventListener('click', adder)
 
@@ -46,4 +72,32 @@ function editor(e) {
     textField.value = txt;
 
     deleteTodo(ele)
+
+    
+}
+function allClear() {    
+    while (place.firstChild) {
+        place.removeChild(place.firstChild);
+    }
+    nodeCount = 0;
+    addCount(nodeCount)
+    todoItems = []
+    setLocal(todoItems)
+}
+
+clearAll.addEventListener('click',allClear);
+
+function addCount(count) {
+    tasks.innerHTML = ` ${count} `;
+}
+
+window.addEventListener('load', (event) => {
+    let todoItems = JSON.parse(window.localStorage.getItem('todo'));
+    for (let i = 0; i < todoItems.length; i++) {
+        adder(todoItems[i])
+    }
+  });
+
+function setLocal(val) {
+    window.localStorage.setItem('todo',JSON.stringify(val))
 }
